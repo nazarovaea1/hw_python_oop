624037a9be6b6245897c0e761f2b4d9d170d790e
Назарова Екатерина <nazarovaea1@gmail.com>
2021-02-05 17:17:16 +0300

    after_revue1

--- homework.py
+++ homework.py
@@ -69,28 +69,3 @@ class CaloriesCalculator(Calculator):
                             f'{today_calories_remained} кКал')
             return answer_calor
         return 'Хватит есть!'
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-calories_calculator = CaloriesCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи должна автоматически
-# добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment="кофе"))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(
-    amount=3000, comment="бар в Танин др", date="08.11.2019"))
-
-calories_calculator.add_record(Record(amount=145, comment="кофе"))
-# и к этой записи тоже дата должна добавиться автоматически
-calories_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-calories_calculator.add_record(Record(
-    amount=3000, comment="бар в Танин др", date="08.11.2019"))
-
-print(cash_calculator.get_today_cash_remained("rub"))
-print(calories_calculator.get_calories_remained())
